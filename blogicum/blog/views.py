8f291821fe583df502cb5de47dd15c4ee3db9255
danilov_alexey <a.danilov@itdept.cloud>
2023-04-12 07:35:34 +0300

    created apps and routes

---
+++ blogicum/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    ...
+    # return render(request=request, status=200)
+
+
+def post_detail(request, id):
+    ...
+
+
+def category_posts(request, category_slug):
+    ...
---
+++ blogicum/pages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def about(request):
+    ...
+
+
+def rules(request):
+    ...
